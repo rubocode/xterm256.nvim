e126519730e9b3c5803529a766a4ab0f18609c2a
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 17:13:35 +0530

    Update cpp.lua

--- lua/ruboterm256/highlights/lang/cpp.lua
+++ lua/ruboterm256/highlights/lang/cpp.lua
@@ -1,3 +1,11 @@
-" C/C++ and other languages like that -------------------------------------{{{
-  "call <sid>X('cCustomParen',           s:uno_4,         '', '')
+-- C/C++ and Other Languages like that
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {
+	cCustomParen = { fg = uno.uno_4, bg = core.Empty, style = sty.Empty },
+}
+
+return M
