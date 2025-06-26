9dfa3917f5ee9e945b2a9b718bf01bab6d23c74e
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 18:03:45 +0530

    Update go.lua

--- lua/ruboterm256/highlights/lang/go.lua
+++ lua/ruboterm256/highlights/lang/go.lua
@@ -1,3 +1,12 @@
-  " Go highlighting ---------------------------------------------------------{{{
-  call <sid>X('goDeclaration',         s:duo_3, '', '')
+-- Go Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {
+	goDeclaration = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+}
+
+return { M }
