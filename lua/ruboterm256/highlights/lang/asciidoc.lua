da227e2bec1c75bf37a79ab32c17673fb54ddf54
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 17:13:32 +0530

    Update asciidoc.lua

--- lua/ruboterm256/highlights/lang/asciidoc.lua
+++ lua/ruboterm256/highlights/lang/asciidoc.lua
@@ -1,3 +1,11 @@
-" Asciidoc highlighting ---------------------------------------------------{{{
-  call <sid>X('asciidocListingBlock',   s:uno_2,  '', '')
+-- Asciidoc Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {
+	asciidocListingBlock = { fg = uno.uno_2, bg = core.Empty, style = sty.Empty },
+}
+
+return M
