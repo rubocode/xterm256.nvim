d451a0caa4a35c7a14b7f9a032b11e8948d5878e
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 19:18:19 +0530

    Update nerdtree.lua

--- lua/ruboterm256/highlights/lang/nerdtree.lua
+++ lua/ruboterm256/highlights/lang/nerdtree.lua
@@ -1,3 +1,16 @@
- " NERDTree highlighting ---------------------------------------------------{{{
-  call <sid>X('NERDTreeExecFile',      s:duo_1, '', '')
+ -- NERDTree Highlighting
+ --
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {}
+
+M.CHANGES = {
+	NERDTreeExecFile = { fg = duo.duo_1, core.Empty, sty.Empty),
+}
+
+N.LINKS = {}
+
+return M
