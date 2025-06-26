d081e8c5b908d765a6571b309fb47e2d897bb280
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 19:30:03 +0530

    Update sass.lua

--- lua/ruboterm256/highlights/lang/sass.lua
+++ lua/ruboterm256/highlights/lang/sass.lua
@@ -1,17 +1,31 @@
-  call <sid>X('sassAmpersand',          s:syntax_accent, '', '')
-  call <sid>X('sassClass',              s:uno_1,         '', '')
-  call <sid>X('sassControl',            s:duo_3,         '', '')
-  call <sid>X('sassExtend',             s:duo_3,         '', '')
-  call <sid>X('sassFor',                s:uno_1,         '', '')
-  call <sid>X('sassProperty',           s:uno_3,         '', '')
-  call <sid>X('sassFunction',           s:duo_1,         '', '')
-  call <sid>X('sassId',                 s:duo_2,         '', '')
-  call <sid>X('sassInclude',            s:uno_1,         '', '')
-  call <sid>X('sassMedia',              s:duo_3,         '', '')
-  call <sid>X('sassMediaOperators',     s:uno_1,         '', '')
-  call <sid>X('sassMixin',              s:duo_3,         '', '')
-  call <sid>X('sassMixinName',          s:duo_2,         '', '')
-  call <sid>X('sassMixing',             s:duo_3,         '', '')
-  call <sid>X('sassVariable',           s:uno_2,         '', '')
-  call <sid>X('sassVariableAssignment', s:uno_4,         '', '')
+-- SASS Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {}
+
+M.CHANGES = {
+	sassAmpersand = { fg = core.syntax_accent, bg = core.Empty, style = sty.Empty },
+	sassClass = { fg = uno.uno_1, bg = core.Empty, style = sty.Empty },
+	sassControl = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+	sassExtend = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+	sassFor = { fg = uno.uno_1, bg = core.Empty, style = sty.Empty },
+	sassProperty = { fg = uno.uno_3, bg = core.Empty, style = sty.Empty },
+	sassFunction = { fg = duo.duo_1, bg = core.Empty, style = sty.Empty },
+	sassId = { fg = duo.duo_2, bg = core.Empty, style = sty.Empty },
+	sassInclude = { fg = uno.uno_1, bg = core.Empty, style = sty.Empty },
+	sassMedia = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+	sassMediaOperators = { fg = uno.uno_1, bg = core.Empty, style = sty.Empty },
+	sassMixin = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+	sassMixinName = { fg = duo.duo_2, bg = core.Empty, style = sty.Empty },
+	sassMixing = { fg = duo.duo_3, bg = core.Empty, style = sty.Empty },
+	sassVariable = { fg = uno.uno_2, bg = core.Empty, style = sty.Empty },
+	sassVariableAssignment = { fg = uno.uno_4, bg = core.Empty, style = sty.Empty },
+}
+
+M.LINKS = {}
+
+return M
