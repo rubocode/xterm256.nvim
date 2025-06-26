58ff320cef6f02559a4a91f5768937008504fdf1
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 19:50:32 +0530

    Update xml.lua

--- lua/ruboterm256/highlights/lang/xml.lua
+++ lua/ruboterm256/highlights/lang/xml.lua
@@ -1,6 +1,19 @@
-" XML highlighting --------------------------------------------------------{{{
-  call <sid>X('xmlAttrib',  s:uno_1,         '', '')
-  call <sid>X('xmlEndTag',  s:syntax_accent, '', '')
-  call <sid>X('xmlTag',     s:syntax_accent, '', '')
-  call <sid>X('xmlTagName', s:syntax_accent, '', '')
+-- XML Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {}
+
+M.ADDITIONS = {
+	xmlAttrib = { fg = uno.uno_1, bg = core.Empty, style = sty.Empty },
+	xmlEndTag = { fg = core.syntax_accent, bg = core.Empty, style = sty.Empty },
+	xmlTag = { fg = core.syntax_accent, bg = core.Empty, style = sty.Empty },
+	xmlTagName = { fg = core.syntax_accent, bg = core.Empty, style = sty.Empty },
+}
+
+M.LINKS = {}
+
+return M
