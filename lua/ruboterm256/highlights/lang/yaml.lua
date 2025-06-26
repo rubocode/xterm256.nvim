f3d84c37d913f0e6a762cab132a3d30a5bac8d41
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 19:55:03 +0530

    Update yaml.lua

--- lua/ruboterm256/highlights/lang/yaml.lua
+++ lua/ruboterm256/highlights/lang/yaml.lua
@@ -1,7 +1,20 @@
- " YAML highlighting -------------------------------------------------------{{{
-  call <sid>X('yamlKey',         s:duo_2, '', '')
-  call <sid>X('yamlOperator',    s:uno_4, '', '')
+-- YAML Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
-  call <sid>X('liquidDelimiter', s:uno_4, '', '')
-  call <sid>X('liquidKeyword',   s:uno_3, '', '')
+local M = {}
 
+M.CHANGES = {
+	yamlKey = { fg = duo.duo_2, bg = core.Empty, style = sty.Empty },
+	yamlOperator = { fg = uno.uno_4, bg = core.Empty, style = sty.Empty },
+
+	liquidDelimiter = { fg = uno.uno_4, bg = core.Empty, style = sty.Empty },
+	liquidKeyword = { fg = uno.uno_3, bg = core.Empty, style = sty.Empty },
+}
+
+M.LINKS = {}
+
+return M
