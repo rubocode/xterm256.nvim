1c725ef1653cdcc50e886901fdef903550b9de73
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 12:46:50 +0530

    can leave style = nil!

---
+++ lua/ruboterm256/highlights/std/syntax.lua
@@ -0,0 +1,46 @@
+-- Standard Syntax Highlighting
+
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
+
+local M = {
+	{ name = "Comment", fg = uno.uno_4, bg = nil, style = sty.italic },
+	{ name = "Constant", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "String", fg = duo.duo_1, bg = nil, style = nil },
+	{ name = "Character", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Number", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Boolean", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Float", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Identifier", fg = uno.uno_3, bg = nil, style = sty.none },
+	{ name = "Function", fg = uno.uno_2, bg = nil, style = nil },
+	{ name = "Statement", fg = duo.duo_1, bg = nil, style = sty.none },
+	{ name = "Conditional", fg = core.syntax_accent, bg = nil, style = nil },
+	{ name = "Repeat", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Label", fg = uno.uno_1, bg = nil, style = nil },
+	{ name = "Operator", fg = core.syntax_accent, bg = nil, style = sty.none },
+	{ name = "Keyword", fg = uno.uno_1, bg = nil, style = nil },
+	{ name = "Exception", fg = uno.uno_1, bg = nil, style = nil },
+	{ name = "PreProc", fg = uno.uno_1, bg = nil, style = nil },
+	-- { name = "Include", fg = duo.duo_2, bg = nil, style = nil },
+	-- { name = "Define", fg = duo.duo_2, bg = nil, style = sty.none },
+	-- { name = "Macro", fg = uno.uno_3, bg = nil, style = nil },
+	-- { name = "PreCondit", fg = aux.Red1, bg = nil, style = nil },
+	{ name = "Type", fg = duo.duo_1, bg = nil, style = sty.none },
+	{ name = "StorageClass", fg = duo.duo_2, bg = nil, style = nil },
+	{ name = "Structure", fg = uno.uno_1, bg = nil, style = nil },
+	{ name = "Typedef", fg = uno.uno_1, bg = nil, style = nil },
+	{ name = "Special", fg = uno.uno_3, bg = nil, style = nil },
+	{ name = "SpecialChar", fg = nil, bg = nil, style = nil },
+	{ name = "Tag", fg = nil, bg = nil, style = nil },
+	{ name = "Delimiter", fg = uno.uno_4, bg = nil, style = nil },
+	{ name = "SpecialComment", fg = nil, bg = nil, style = nil },
+	{ name = "Debug", fg = nil, bg = nil, style = nil },
+	{ name = "Underlined", fg = duo.duo_1, bg = nil, style = sty.underline },
+	{ name = "Ignore", fg = nil, bg = nil, style = nil },
+	{ name = "Error", fg = core.syntax_color_removed, bg = core.syntax_bg, style = sty.bold },
+	{ name = "Todo", fg = core.syntax_color_added, bg = core.syntax_bg, style = nil },
+}
+
+return M
