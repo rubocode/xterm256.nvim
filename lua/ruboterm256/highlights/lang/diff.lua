cc04e29c76a21353de94c6d9d024f20ae00316b0
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-26 17:37:25 +0530

    Update diff.lua

--- lua/ruboterm256/highlights/lang/diff.lua
+++ lua/ruboterm256/highlights/lang/diff.lua
@@ -1,11 +1,20 @@
-" Diff highlighting -------------------------------------------------------{{{
-  call <sid>X('DiffAdd',     s:syntax_color_added,    s:syntax_selection, '')
-  call <sid>X('DiffChange',  s:syntax_color_modified, s:syntax_selection, '')
-  call <sid>X('DiffDelete',  s:syntax_color_removed,  s:syntax_selection, '')
-  call <sid>X('DiffText',    s:uno_2,                 s:syntax_selection, '')
-  call <sid>X('DiffAdded',   s:duo_2,                 s:syntax_selection, '')
-  call <sid>X('DiffFile',    s:syntax_accent,         s:syntax_selection, '')
-  call <sid>X('DiffNewFile', s:duo_2,                 s:syntax_selection, '')
-  call <sid>X('DiffLine',    s:uno_2,                 s:syntax_selection, '')
-  call <sid>X('DiffRemoved', s:syntax_accent,         s:syntax_selection, '')
+-- Diff Highlighting
+--
+local core = require("ruboterm256.palette.std.core")
+local uno = require("ruboterm256.palette.std.uno")
+local duo = require("ruboterm256.palette.std.duo")
+local sty = require("ruboterm256.palette.std.style")
 
+local M = {
+	DiffAdd = { fg = core.syntax_color_added, bg = core.syntax_selection, style = sty.Empty },
+	DiffChange = { fg = core.syntax_color_modified, bg = core.syntax_selection, style = sty.Empty },
+	DiffDelete = { fg = core.syntax_color_removed, bg = core.syntax_selection, style = sty.Empty },
+	DiffText = { fg = uno.uno_2, bg = core.syntax_selection, style = sty.Empty },
+	DiffAdded = { fg = duo.duo_2, bg = core.syntax_selection, style = sty.Empty },
+	DiffFile = { fg = core.syntax_accent, bg = core.syntax_selection, style = sty.Empty },
+	DiffNewFile = { fg = duo.duo_2, bg = core.syntax_selection, style = sty.Empty },
+	DiffLine = { fg = uno.uno_2, bg = core.syntax_selection, style = sty.Empty },
+	DiffRemoved = { fg = core.syntax_accent, bg = core.syntax_selection, style = sty.Empty },
+}
+
+return M
