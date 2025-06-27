a8e8bad7cbf56d65e8680c48372268ea8cba14c9
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-27 10:39:50 +0530

    Update init.lua

--- lua/xterm256/init.lua
+++ lua/xterm256/init.lua
@@ -15,17 +15,17 @@ function M.setup()
 		local group = k
 		-- print(group, v)
 		-- if group == "Normal" then
-		print(group, k, v.fg, v.bg, v.style)
+		-- print(group, k, v.fg, v.bg, v.style)
 		if v.fg then
-			print(group, ": fg: ", v.fg.i, v.fg.hex)
+			vim.notify(group .. ": fg: " .. v.fg.i .. " " .. v.fg.hex)
 			vim.api.nvim_set_hl(0, group, { fg = v.fg.hex, ctermfg = v.fg.i })
 		end
 		if v.bg then
-			print(group, ": bg: ", v.bg.i, v.bg.hex)
+			vim.notify(group .. " bg: " .. v.bg.i .. " " .. v.bg.hex)
 			vim.api.nvim_set_hl(0, group, { bg = v.bg.hex, ctermbg = v.bg.i })
 		end
 		if v.style then
-			print(group, ": style: ", v.style.text)
+			vim.notify(group .. "style: " .. v.style.text)
 			vim.api.nvim_set_hl(0, group, { [v.style.text] = true })
 		end
 	end
