8bc2a9fda00eeb17964aef444444d7f1e529914f
Ruchira Bomiriya <rubocode@gmail.com>
2025-06-27 11:06:21 +0530

    Update core.lua

--- lua/xterm256/palette/std/core.lua
+++ lua/xterm256/palette/std/core.lua
@@ -6,17 +6,17 @@ local aux = require("xterm256.palette.xterm256.auxiliary")
 local uno = require("xterm256.palette.std.uno")
 
 local M = {
-	syntax_color_renamed = aux.Grey23, -- "#33a0ff"
-	syntax_color_added = aux.Grey27, -- "#43d08a"
-	syntax_color_modified = aux.Grey89, -- "#e0c285"
-	syntax_color_removed = aux.DarkOliveGreen1, -- "#e05252"
+	syntax_color_renamed = aux.DodgerBlue1,
+	syntax_color_added = aux.Chartreuse2,
+	syntax_color_modified = aux.Gold1,
+	syntax_color_removed = aux.OrangeRed1,
 	syntax_fg = uno.uno_2,
 	syntax_bg = aux.Grey3,
-	syntax_accent = aux.DarSeaGreen4, -- "#56b6c2"
-	syntax_gutter = aux.SteelBlue1, -- "#636d83"
-	syntax_selection = aux.Grey85, -- "#dddddd"
-	syntax_fold_bg = aux.Orange4, -- "#5c6370"
-	syntax_cursor_line = aux.Red3, -- "#2c323c"
+	syntax_accent = aux.Turquoise2,
+	syntax_gutter = aux.Grey39,
+	syntax_selection = aux.Grey15,
+	syntax_fold_bg = aux.Grey35,
+	syntax_cursor_line = aux.Grey11,
 }
 
 return M
